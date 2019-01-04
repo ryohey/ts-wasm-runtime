b7c8e68f7546dc879cd9dc926905fbab662c140c
ryohey <info@codingcafe.jp>
2019-01-04 20:01:28 +0900

    Add get_local tests

--- src/wasm-vm/spec/local_get.test.ts
+++ src/wasm-vm/spec/local_get.test.ts
@@ -4,11 +4,15 @@ describe("local_get", () => {
   it("test", () => {
     runTests(
       `(module
-        (func (export "type-local-i32") (result i32) (local i32)
-            get_local 0
-        )
+        (func (export "type-local-i32") (result i32) (local i32) get_local 0)
+        (func (export "type-local-i64") (result i64) (local i64) get_local 0)
+        (func (export "type-local-f32") (result f32) (local f32) get_local 0)
+        (func (export "type-local-f64") (result f64) (local f64) get_local 0)
       )
       (assert_return (invoke "type-local-i32") (i32.const 0))
+      (assert_return (invoke "type-local-i64") (i64.const 0))
+      (assert_return (invoke "type-local-f32") (f32.const 0))
+      (assert_return (invoke "type-local-f64") (f64.const 0))
       `
     )
   })
