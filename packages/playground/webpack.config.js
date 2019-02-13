7f60f30cbb424482f013b685fd45bcfecdb71cfd
ryohey <info@codingcafe.jp>
2019-02-14 00:56:08 +0900

    Build playground to docs directory

--- packages/playground/webpack.config.js
+++ packages/playground/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "../../docs")
   },
   plugins: [
     new HtmlWebpackPlugin(),
