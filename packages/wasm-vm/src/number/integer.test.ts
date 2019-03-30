78292c9d7d273b23b5ea86ecb5372117dcb1a274
ryohey <info@codingcafe.jp>
2019-03-30 11:20:01 +0900

    Fix test name

--- packages/wasm-vm/src/number/integer.test.ts
+++ packages/wasm-vm/src/number/integer.test.ts
@@ -1,7 +1,7 @@
 import { asSigned } from "./integer"
 
-describe("unsigned", () => {
-  it("asUnsigned", () => {
+describe("integer", () => {
+  it("asSigned", () => {
     expect(asSigned(0b1000_0000, 8)).toBe(-128)
     expect(asSigned(0b0111_1111, 8)).toBe(127)
   })
